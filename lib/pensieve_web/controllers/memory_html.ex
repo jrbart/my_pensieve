14c0d470588a79ef7532e7210752250d8819e691
jrbart <jorabart@gmail.com>
2024-11-12 06:34:03 -0600

    extract MemrpyHTML.memory_inserted_at/1

--- lib/pensieve_web/controllers/memory_html.ex
+++ lib/pensieve_web/controllers/memory_html.ex
@@ -3,6 +3,12 @@ defmodule PensieveWeb.MemoryHTML do
 
   embed_templates "/memory_html/*"
 
+  alias Pensieve.Memories.Memory
+
+  def memory_inserted_at(%Memory{inserted_at: timestamp}) do
+    Calendar.strftime(timestamp, "%m/%d/%Y %I:%M%p")
+  end
+
   attr :memories, :list, required: true
   def information(assigns) do
     ~H"""
